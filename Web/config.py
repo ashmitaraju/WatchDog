f2080d0a44fdab30c2f6705511781f1ac9dcae91
CMS <ashmita.raju@gmail.com>
2018-02-16 00:36:38 +0530

    updated requirements

--- Web/config.py
+++ Web/config.py
@@ -1,6 +1,8 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'watchdog-secret-key'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/watchdog'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/watchdog'
+SQLALCHEMY_DATABASE_URI = 'mysql://aravindbs@watchdogserver:Watchdog123@watchdogserver.mysql.database.azure.com:3306/watchdog'
+
 UPLOADS_DEFAULT_DEST = 'app/static/img/'
 UPLOADS_DEFAULT_URL = '../static/img/'
 UPLOADED_IMAGES_ALLOW = set(['png', 'jpg', 'jpeg'])
