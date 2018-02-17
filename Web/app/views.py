77f0be5c3255f1746ec8edc8731d04063b5f8a52
CMS <ashmita.raju@gmail.com>
2018-02-17 16:20:14 +0530

    changes

--- Web/app/views.py
+++ Web/app/views.py
@@ -42,7 +42,7 @@ def signup():
         print code
 
         login_user(user)
-        flash('Registration Successful! You may now create your profile.')
+        flash('Registration Successful! You may now create your profile.' , 'success')
         return redirect(url_for('editProfile'))
     return render_template('signup.html', title='Sign Up', form = form)
 
@@ -120,8 +120,9 @@ def cameraDetails():
 @app.route('/unAuth' , methods = ['GET' , 'POST'])
 @login_required
 def unAuth():
-    form = EditImageGalleryForm()
-    return render_template('Auth.html', form = form)
+    
+    pics = UnauthImageGallery.query.filter_by(username = current_user.username).all()
+    return render_template('Unauth.html', pics = pics)
 
 @app.route('/train' , methods = ['GET' , 'POST'])
 @login_required
@@ -229,12 +230,13 @@ def addPics(user):
     current_person = Persons.query.filter_by(person_id = user).first() 
 
     form = EditImageGalleryForm()
-
+    """
     if form.picture.data: 
         print "picture"
         naam = current_person.person_name
-        takePicture(naam)
-    
+        takePicture(naam)   
+    """
+
     if form.skip.data: 
         
         if 'image' in request.files:
--- Web/config.py
+++ Web/config.py
@@ -9,3 +9,8 @@ UPLOADED_IMAGES_ALLOW = set(['png', 'jpg', 'jpeg'])
 
 UPLOADED_IMAGES_DEST ='app/static/img/'
 UPLOADED_IMAGES_URL = '../static/img/'
+
+AZURE_STORAGE_ACCOUNT_NAME = "sokvideoanalyze8b05"
+AZURE_STORAGE_ACCOUNT_KEY = "4SdxwWwId8+nPEhD6yY4f6om1BGnlbFAp7EnUcyrKcxKNOVTtDwJ6syOQz7ZMrvewWTyQWBBYd5Jc7WcBE1D9g=="
+AZURE_STORAGE_CONTAINER_NAME = "static"  # make sure the container is created. Refer to the previous examples or to the Azure admin panel
+AZURE_STORAGE_DOMAIN = 'https://sokvideoanalyze8b05.blob.core.windows.net'
