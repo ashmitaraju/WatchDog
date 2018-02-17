7109961e13b4dc3ae5b38283d206201be449800d
ashmita <ashmita.raju@gmail.com>
2018-02-17 13:56:44 +0000

    commits

--- Web/app/__init__.py
+++ Web/app/__init__.py
@@ -13,9 +13,9 @@ login_manager = LoginManager()
 app = Flask(__name__)
 app.config.from_object('config')
 
-#app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 
+#app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 *
 app.secret_key = 'red-wedding'
-#images = Images(app) 
+#images = Images(app)
 
 db.init_app(app)
 login_manager.init_app(app)
@@ -28,7 +28,7 @@ Bootstrap(app)
 images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
 
-#app = Flask(__name__, static_url_path="https://sokvideoanalyze8b05.blob.core.windows.net/static/", static_folder="static") 
+#app = Flask(__name__, static_url_path="https://sokvideoanalyze8b05.blob.core.windows.net/static/", static_folder="static")
 
 
 
