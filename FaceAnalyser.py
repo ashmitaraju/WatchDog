9d27b8764d5158b6f4203892d3f6dcda2b391db1
Anirudh Konduru <anirudhmkonduru@gmail.com>
2018-04-14 16:21:50 +0530

    added windowManager, moved faceAnalyser, sendFaces, removed ipstream, multiple fixes

---
+++ FaceAnalyser.py
@@ -0,0 +1,84 @@
+from faceVerify import face_verify
+import re
+import datetime
+from azure.storage.blob import BlockBlobService
+import MySQLdb
+import yaml
+from multiprocessing import Process
+
+with open("config.yaml", "r") as f:
+    config = yaml.load(f)
+
+
+class FaceAnalyser(object):
+    def __init__(self, user_name, cam_ID, cam_location):
+
+        self.user_name = user_name
+        self.cam_ID = cam_ID
+        self.cam_location = cam_location
+        self.block_blob_service = BlockBlobService(account_name=config['azure-blob']['account_name'],
+                                                   account_key=config['azure-blob']['account_key'],
+                                                   )
+        sql = config['mysql']
+        self.db = MySQLdb.connect(sql['server'], sql['username'], sql['password'], sql['database'])
+        self.cursor = self.db.cursor()
+
+        query = """select azure_id, person_name from Persons, AuthImageGallery
+                 where Persons.person_id=AuthImageGallery.person_id 
+                 and Persons.username= '%s'""" % user_name
+
+        self.cursor.execute(query)
+
+        results = self.cursor.fetchall()
+
+        self.database = {}
+        for result in results:
+            self.database[result[0]] = result[1]
+
+
+def analyser(response_face_queue, user_name, cam_ID, cam_location):
+
+    face_analyser = FaceAnalyser(user_name, cam_ID, cam_location)
+
+    while True:
+        while response_face_queue.empty():
+            pass
+        response = response_face_queue.get()
+        face_ids = []
+        if response[0]:
+            for face in response[0]:
+                print face
+                fid = str(face["faceId"])
+                # print fid
+                face_ids.append(fid)
+                verified = face_verify(face_ids, face_analyser.user_name)
+                # print type ( verified )
+                found = None
+                if verified:
+                    for verifiedFace in verified:
+                        # print type (verifiedFace)
+                        if verifiedFace["candidates"]:
+                            for candidate in verifiedFace["candidates"]:
+                                found = candidate["personId"]
+                            if found in face_analyser.database:
+                                print face_analyser.database[found]
+
+                        else:
+                            timestamp = str(datetime.now())
+                            filename = re.sub(r' ', '_', timestamp)
+                            # print filename
+
+                            face_analyser.block_blob_service.create_blob_from_bytes('unauthorized', filename, response[1])
+                            url = "https://watchdogsok.blob.core.windows.net/unauthorized/%s" % filename
+                            query_send_unauth = """
+insert into UnauthImageGallery(username, image_filename, image_path, timestamp, cameraID, cameraLocation) 
+values('%s', '%s', '%s' , '%s', '%s', '%s')""" % (face_analyser.user_name, filename, url, timestamp, face_analyser.cam_ID, face_analyser.cam_location)
+                            # query = """select * from Persons"""
+                            # print query
+                            face_analyser.cursor.execute(query_send_unauth)
+                            face_analyser.db.commit()
+                            # print cursor.fetchall()
+                            print "Face not authorized"
+        else:
+            pass
+            # print "No faces in frame"
--- frameGrabber.py
+++ frameGrabber.py
@@ -1,18 +1,16 @@
 import cv2
 import yaml
-
+from WindowManager import WindowManager
 
 with open("config.yaml", "r") as f:
     config = yaml.load(f)
 
 # time.sleep(2)
 
-
-def get_frame(cam):
+def get_frame(cam, windowManager):
     # print "hi"
-
     img = cam.read()[1]
     img = cv2.flip(img, 1)
-    cv2.imshow("Cam Frames", img)
+    windowManager.put(img)
     cv2.waitKey(50)
     return img
